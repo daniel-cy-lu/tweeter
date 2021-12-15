8fc95f758e5d2f2cd20c0c552cd897dbcceb7ce5
Daniel Lu <daniel_lu@hotmail.com>
2021-12-15 10:10:58 -0500

    add chatbox word count and make red if word is negative count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+
+$('textarea').keyup(function() {
+    
+  let characterCount = $(this).val().length;
+  let remaining = $('#remaining');
+  let remainingNum = 140 - characterCount;
+  if (remainingNum < 0) {
+    remaining.css('color', '#F00001');
+  }
+  remaining.text(remainingNum);
+  
+});
